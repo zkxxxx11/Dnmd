d5b26ed453081ed760bc6757a9b481846aa761c0
zkxxx <zhangk@servyou.com.cn>
2021-01-31 12:35:54 +0800

    <feat>第一次提交

---
+++ utils/get_ip.py
@@ -0,0 +1,21 @@
+import requests
+
+from utils.log4 import Log
+
+logger = Log(name='get_ip.log').logger
+
+
+def get_random_proxy(retry_time):
+    if retry_time < 5:
+        url = '	http://api.shenlongip.com/ip'
+        params = {
+            'key':'walfjgse',
+            'pattern':'txt',
+            'count':'1',
+            'protocol':'1'
+        }
+        html = requests.get(url, params=params)
+        print(html.text.strip())
+        return str(html.text.strip())
+    else:
+        logger('ip 不行了 失败超出设定次数')
