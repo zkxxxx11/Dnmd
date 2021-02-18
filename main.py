2f9012a67d31c4ee8de89a63de022f214f9db515
zkxxx <zhangk@servyou.com.cn>
2021-02-18 11:07:38 +0800

    <feat>多线程运行

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+import threading
+
 from dnmd_demo import Dnmd
 import multiprocessing as mp
 from multiprocessing import Pool,Lock
 from concurrent.futures import ProcessPoolExecutor
 if __name__ == '__main__':
     user_infos = {
-        'zk':{'id':'xxxxxxxxxxxxxxxx',
-              'psw':'xxxxxxxxxxx'},
+        'zk':{'id':'xxxxxxx',
+              'psw':'xxxxxxx',
+              'email':''},
 
     }
-    work_count = 3
-    # with ProcessPoolExecutor(work_count) as pool:
-    #     for i, j in enumerate(user_infos):
-    #         inner_info = user_infos[j]
-    #         print(i, user_infos[j])
-    #         pool.submit(s.login(inner_info.get('id'), inner_info.get('psw')))  # 执行
-    pool = Pool(1)
-    s = Dnmd()
+    # s = Dnmd()
+    #
+    # for user_info in user_infos:
+    #     inner_info = user_infos[user_info]
+    #     print(inner_info)
+    #     # pool.apply_async(s.login(inner_info.get('id'), inner_info.get('psw')))
+    #     # s.login(inner_info.get('id'), inner_info.get('psw'))
+    #     s.login(**inner_info)
+    #
+    threads = []
     for user_info in user_infos:
         inner_info = user_infos[user_info]
         print(inner_info)
-        pool.apply_async(s.login(inner_info.get('id'), inner_info.get('psw')))
-    # 31407
-    # s.login(id, psw)
+        s = Dnmd()
+        threads.append(threading.Thread(target=s.login, kwargs=inner_info))
+    for t in threads:
+        t.start()
